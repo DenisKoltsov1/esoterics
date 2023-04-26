44fcb02bc367f20aefd2d74651b2ea88f259c59d
AleksandrNikiforov <aleksandr040988@gmail.com>
2023-04-26 14:26:36 +0500

    add test

--- site_esoterics/news/tests.py
+++ site_esoterics/news/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import News
 
-# Create your tests here.
+
+class TestNews(TestCase):
+
+    def setUp(self):
+        self.news = News.objects.create(title='Новая новость', content='Содержимое новости')
+        print('Я выполняюсь перед каждым тестом')
+
+    def tearDown(self):
+        News.objects.all().delete()
+        print('Я выполняюсь после каждого теста')
+
+    def test_init(self):
+        self.assertEqual(self.news.title, 'Новая новость')
+        self.assertEqual(self.news.content, 'Содержимое новости')
